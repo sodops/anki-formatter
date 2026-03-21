d8369cbcf18c934b64a63660309be42b36a9675a
Sodiq <sodiq@nixos>
2026-03-21 17:12:01 +0500

    noting

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,11 +1,11 @@
 import "@testing-library/jest-dom";
 
 // Polyfill for Next.js server components
-if (typeof Request === 'undefined') {
+if (typeof Request === "undefined") {
   global.Request = class Request {} as any;
 }
 
-if (typeof Response === 'undefined' || !Response.json) {
+if (typeof Response === "undefined" || !Response.json) {
   global.Response = class Response {
     body?: any;
     init?: ResponseInit;
@@ -14,21 +14,27 @@ if (typeof Response === 'undefined' || !Response.json) {
       this.body = body;
       this.init = init;
     }
-    
+
     static json(data: any, init?: ResponseInit) {
       return new Response(JSON.stringify(data), {
         ...init,
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
           ...(init?.headers || {}),
         },
       });
     }
-    
+
     async json() {
-      return typeof this.body === 'string' ? JSON.parse(this.body) : this.body;
+      return typeof this.body === "string" ? JSON.parse(this.body) : this.body;
     }
-    
+
     get status() {
       return this.init?.status || 200;
     }
+  } as any;
+}
+
+if (typeof Headers === "undefined") {
+  global.Headers = class Headers {} as any;
+}
